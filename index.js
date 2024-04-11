432368b92d1a4163a188f988f9d5e5cb1e9e4e24
Sharadindu Das <remodas7774@gmail.com>
2024-04-11 19:31:15 +0530

    updated code

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import cors from 'cors';
-import dotenv from 'dotenv';
-dotenv.config()
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.use(express.json());
 app.use(cors());
